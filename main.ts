3ef77dfe1fc4349fed1df486080a8152213a5574
Obilolo <78688812+Obilolo@users.noreply.github.com>
2021-10-24 19:35:14 +0200

    nouvelles actions

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showIcon(IconNames.EigthNote)
+    music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
+})
+input.onSound(DetectedSound.Loud, function () {
+    basic.showIcon(IconNames.Sad)
+})
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
     music.playMelody("E B C5 A B G A F ", 140)
 })
