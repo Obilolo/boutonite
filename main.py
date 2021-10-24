2b2cf5d322f3ae60b633ed2ea01f4db79e7db523
Obilolo <78688812+Obilolo@users.noreply.github.com>
2021-10-24 16:00:55 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.py
@@ -0,0 +1,31 @@
+def on_logo_touched():
+    soundExpression.spring.play()
+input.on_logo_event(TouchButtonEvent.TOUCHED, on_logo_touched)
+
+def on_button_pressed_a():
+    basic.show_leds("""
+        . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+    """)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    basic.clear_screen()
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    basic.show_leds("""
+        . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+    """)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_gesture_shake():
+    basic.show_icon(IconNames.SKULL)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
---
+++ main.ts
@@ -0,0 +1,27 @@
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    music.playMelody("E B C5 A B G A F ", 140)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Skull)
+})
